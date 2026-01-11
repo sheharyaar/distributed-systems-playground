cd5ff342f64caaaca339b47b3b44f3bf84ab41c2
Mohammad Shehar Yaar Tausif <sheharyaar48@gmail.com>
2026-01-12 00:15:04 +0530

    restructure dirs
    Signed-off-by: Mohammad Shehar Yaar Tausif <sheharyaar48@gmail.com>

---
+++ flyio-challenges/broadcast/02-multi-node/multiple-unicast.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"log"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+var arr []int
+
+func main() {
+	n := maelstrom.NewNode()
+	if n == nil {
+		log.Fatal("error in craeting node")
+		return
+	}
+
+	n.Handle("broadcast", func(m maelstrom.Message) error {
+		var body map[string]any
+
+		if err := json.Unmarshal(m.Body, &body); err != nil {
+			return err
+		}
+
+		val, ok := body["message"].(float64)
+		if !ok {
+			return errors.New("invalid value in broadcast message")
+		}
+
+		arr = append(arr, int(val))
+
+		reply := map[string]string{
+			"type": "broadcast_ok",
+		}
+
+		if err := n.Reply(m, reply); err != nil {
+			return err
+		}
+
+		for _, node := range n.NodeIDs() {
+			// send a nodecast
+			msg := map[string]any{
+				"type":    "nodecast",
+				"message": val,
+			}
+
+			n.Send(node, msg)
+		}
+
+		return nil
+	})
+
+	// this is looped unicast, this cannot ensure serializability
+	n.Handle("nodecast", func(m maelstrom.Message) error {
+		var body map[string]any
+
+		if err := json.Unmarshal(m.Body, &body); err != nil {
+			return err
+		}
+
+		val, ok := body["message"].(float64)
+		if !ok {
+			return errors.New("invalid value in nodecast message")
+		}
+
+		arr = append(arr, int(val))
+		return nil
+	})
+
+	n.Handle("read", func(m maelstrom.Message) error {
+		var body map[string]any
+
+		if err := json.Unmarshal(m.Body, &body); err != nil {
+			return err
+		}
+
+		reply := map[string]any{
+			"type":     "read_ok",
+			"messages": arr,
+		}
+		return n.Reply(m, reply)
+	})
+
+	n.Handle("topology", func(m maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(m.Body, &body); err != nil {
+			return err
+		}
+
+		reply := map[string]string{
+			"type": "topology_ok",
+		}
+
+		return n.Reply(m, reply)
+	})
+
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+}
